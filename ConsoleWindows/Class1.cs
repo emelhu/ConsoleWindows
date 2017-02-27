8b519cbb934790214819da70aad5fa49b52fb94f
eMeL <emel@emel.hu>
2017-02-27 19:43:36 +0100

    Add project files.

---
+++ ConsoleWindows/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleWindows
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
